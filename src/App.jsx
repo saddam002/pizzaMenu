2390764c6807d241e5a044326f29528d45abdb0e
saddam002 <mohammadsaddamali98@gmail.com>
2026-02-16 23:11:56 +0545

    adding css and making header section and styling our menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import Pizza from "./components/Pizza";
+import "./app.css";
 function App() {
   return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="company-name">
+      <h1>Fast React Pizza Co.</h1>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
+
+function Footer() {
+  return (
     <>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <footer>{new Date().toLocaleTimeString()}----We'r currently open.</footer>
     </>
   );
 }
